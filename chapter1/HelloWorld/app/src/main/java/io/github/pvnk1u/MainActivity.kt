68f6d45c548da4893f6b5a6f26b4d38f2f8a4169
pvnk1u <pvnk1u@gmail.com>
2024-03-13 23:49:45 +0800

    HelloWorld Demo

---
+++ chapter1/HelloWorld/app/src/main/java/io/github/pvnk1u/MainActivity.kt
@@ -0,0 +1,51 @@
+package io.github.pvnk1u
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.pvnk1u.ui.theme.HelloWorldTheme
+
+// 继承自ComponentActivity，所有自定义的Activity都必须继承它或者它的子类才能拥有Activity的特性
+class MainActivity : ComponentActivity() {
+    // Activity被创建时必定要执行的代码
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloWorldTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+        // 打印debug级别的日志,第一个参数一般传入当前的类名，用于对打印信息进行过滤
+        Log.d("MainActivity","onCreate execute")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloWorldTheme {
+        Greeting("Android")
+    }
+}
