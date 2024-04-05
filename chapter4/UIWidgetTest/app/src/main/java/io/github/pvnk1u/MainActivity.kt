a24b74f62022a003a08a4808fc6af371207f2e0c
pvnk1u <pvnk1u@gmail.com>
2024-04-05 21:14:04 +0800

    TextView和Button控件的笔记、代码

---
+++ chapter4/UIWidgetTest/app/src/main/java/io/github/pvnk1u/MainActivity.kt
@@ -0,0 +1,20 @@
+package io.github.pvnk1u
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button : Button  = findViewById(R.id.button)
+        button.setOnClickListener{
+            // 点击逻辑
+        }
+    }
+}
