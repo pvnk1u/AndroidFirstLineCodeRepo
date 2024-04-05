2df6c03ff47672132695ad6a6f9e199adbbadc64
pvnk1u <pvnk1u@gmail.com>
2024-04-05 21:31:11 +0800

    Button控件和EditText控件的笔记、代码

--- chapter4/UIWidgetTest/app/src/main/java/io/github/pvnk1u/MainActivity.kt
+++ chapter4/UIWidgetTest/app/src/main/java/io/github/pvnk1u/MainActivity.kt
@@ -3,18 +3,43 @@ package io.github.pvnk1u
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button : Button  = findViewById(R.id.button)
+        /**
+         * 直接通过lambda表达式指定按钮点击事件的形式和当前Activity实现View.OnClickListener接口，
+         * 并通过setOnClickListener方法指定当前Activity的两种方式都可以实现按钮的点击事件回调动作
+         */
         button.setOnClickListener{
-            // 点击逻辑
+            // 点击逻辑,实现点击按钮时获取InputText中的输入内容并通过Toast弹窗形式弹框显示
+            val editText: EditText = findViewById(R.id.editText)
+            val inputText =  editText.text.toString()
+            Toast.makeText(this,inputText,Toast.LENGTH_SHORT).show()
+        }
+        button.setOnClickListener(this)
+    }
+
+
+    /**
+     * 实现View.OnClickListener
+     */
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.button -> {
+                // 点击逻辑,实现点击按钮时获取InputText中的输入内容并通过Toast弹窗形式弹框显示
+                val editText: EditText = findViewById(R.id.editText)
+                val inputText =  editText.text.toString()
+                Toast.makeText(this,inputText,Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
