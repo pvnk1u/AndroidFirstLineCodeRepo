d8b9873f9be0ef67061ed040df284f1ac13a3ba7
pvnk1u <pvnk1u@gmail.com>
2024-04-07 22:10:40 +0800

    LinearLayout布局和RelativeLayout布局的代码、笔记

--- chapter4/UILayoutTest/app/src/main/java/io/github/pvnk1u/MainActivity.kt
+++ chapter4/UILayoutTest/app/src/main/java/io/github/pvnk1u/MainActivity.kt
@@ -1,14 +1,11 @@
 package io.github.pvnk1u
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_relative_layout_to_other)
     }
 }
