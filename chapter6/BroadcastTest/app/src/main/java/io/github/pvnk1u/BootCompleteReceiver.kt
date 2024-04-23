0ad887e20e17efbd1beee283a5becb3a9af991e6
pvnk1u <pvnk1u@gmail.com>
2024-04-23 20:08:03 +0800

    静态注册实现开机启动

---
+++ chapter6/BroadcastTest/app/src/main/java/io/github/pvnk1u/BootCompleteReceiver.kt
@@ -0,0 +1,14 @@
+package io.github.pvnk1u
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+
+class BootCompleteReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Toast.makeText(context,"Boot Complete",Toast.LENGTH_SHORT).show()
+    }
+}
