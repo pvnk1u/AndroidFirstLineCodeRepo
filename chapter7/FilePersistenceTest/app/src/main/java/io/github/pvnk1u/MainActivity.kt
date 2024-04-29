0a811b3b8a32b9761c7dd219a6c5827d85bc5d17
pvnk1u <pvnk1u@gmail.com>
2024-04-29 18:37:49 +0800

    输入库内容保存到文件中

--- chapter7/FilePersistenceTest/app/src/main/java/io/github/pvnk1u/MainActivity.kt
+++ chapter7/FilePersistenceTest/app/src/main/java/io/github/pvnk1u/MainActivity.kt
@@ -1,11 +1,43 @@
 package io.github.pvnk1u
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import java.io.BufferedWriter
+import java.io.IOException
+import java.io.OutputStreamWriter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * 重写onDestroy方法，保证在Activity销毁之前一定会调用到这个方法
+     *
+     * 销毁前获取editText中的内容并保存到文件中
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+        val editText : EditText = findViewById(R.id.editText)
+        val inputText = editText.text.toString()
+        save(inputText)
+    }
+
+    /**
+     * 将输入框中的文本保存到文件中
+     */
+    private fun save(inputText:String){
+        try {
+            val output = openFileOutput("data",Context.MODE_PRIVATE)
+            val writer = BufferedWriter(OutputStreamWriter(output))
+            writer.use {
+                it.write(inputText)
+            }
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
     }
 }
