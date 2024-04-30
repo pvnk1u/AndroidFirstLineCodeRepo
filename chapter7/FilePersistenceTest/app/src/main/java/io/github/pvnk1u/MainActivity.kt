25d4175f0bdbef0d0ce7e6e236dec40d69e8bd94
pvnk1u <pvnk1u@gmail.com>
2024-04-30 12:51:52 +0800

    从文件中读取数据

--- chapter7/FilePersistenceTest/app/src/main/java/io/github/pvnk1u/MainActivity.kt
+++ chapter7/FilePersistenceTest/app/src/main/java/io/github/pvnk1u/MainActivity.kt
@@ -3,15 +3,49 @@ package io.github.pvnk1u
 import android.content.Context
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.IOException
+import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /**
+         * 应用重启后重新加载onDestroy时保存的内容
+         * */
+        val editText : EditText = findViewById(R.id.editText)
+        val inputText = load()
+        if (inputText.isNotEmpty()){
+            editText.setText(inputText)
+            // 调用setSelection()方法将输入光标移动到文本的末尾位置以便继续输入
+            editText.setSelection(inputText.length)
+            Toast.makeText(this,"Restoring succeeded",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    /**
+     * 从文件中读取数据
+     */
+    private fun load(): String {
+        val content = StringBuilder()
+        try {
+            // 将会读取/data/data/<package name>/files/data 文件中的内容
+            val input = openFileInput("data")
+            val reader = BufferedReader(InputStreamReader(input))
+            reader.use {
+                reader.forEachLine {
+                    content.append(it)
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return content.toString()
     }
 
     /**
